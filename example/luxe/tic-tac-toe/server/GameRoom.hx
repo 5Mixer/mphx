343f6d5ebd179738e323bf11ca27a3f1ab6eb86d
Anders Nissen <andnis@gmail.com>
2016-02-27 23:30:42 +0100

    Fixed logic for determining the winner

--- example/luxe/tic-tac-toe/server/GameRoom.hx
+++ example/luxe/tic-tac-toe/server/GameRoom.hx
@@ -52,13 +52,13 @@ class GameRoom extends Room {
             tiles[pos.y][pos.x] = current_player;
             turn++;
             broadcast('move', { symbol: symbols[current_player], pos: pos });
-            current_player = (current_player + 1) % required_players;
             if (has_won(current_player)) {
                 for (i in 0 ... connections.length) {
                     var client = connections[i];
                     client.send((i == current_player) ? 'won' : 'lost');
                 }
             } else {
+                current_player = (current_player + 1) % required_players;
                 connections[current_player].send('start_turn', { remove_symbol: (turn >= 6 ? symbols[current_player] : '') });
             }
         }
