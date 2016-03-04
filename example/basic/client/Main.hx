c6d59e0f86d93ce6a8d1aa04f8f543f768bdcc73
5Mixer <dantheman41@live.com.au>
2016-03-04 17:47:10 +1100

    Just modified the example a little, so it can run easily on online servers.

--- example/basic/client/Main.hx
+++ example/basic/client/Main.hx
@@ -5,7 +5,9 @@ class Main
 {
 	var clientSocket:mphx.client.Client;
 	public function new () {
-		clientSocket = new mphx.client.Client("127.0.0.1",8000);
+		var ip = "127.0.0.1";
+		if (Sys.args()[1] != null) ip = Sys.args()[1];
+		clientSocket = new mphx.client.Client(ip,8000);
 
 		clientSocket.connect();
 
